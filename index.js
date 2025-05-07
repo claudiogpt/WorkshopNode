cc30f1b1838aaad9d3dcb5c5aba78a58bd38c95f
claudiogpt <claudioangontijo@gmail.com>
2025-05-07 17:13:20 -0300

    feat: implement express server with basic routes and middleware

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log("Hello, world!");
+//Importa o módulo express
+const express = require('express');
+
+// Importando o módulo express
+const app = express();
+
+// Configurando o middleware para analisar o corpo das requisições
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(new Date().toISOString(), req.host, req.method, req.url);
+    next(); 
+});
+
+// Cria um manipulador da rota para a raiz da aplicação
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/claudio', (req, res) => {
+  res.send('Cláudio Augusto Novaes Gontijo');
+});
+
+app.get('/ola', (req, res) => {
+  let nome = req.query.nome;
+  res.send(`Olá ${nome}`);
+});
+
+const produtos = [
+  { id: 1, nome: 'Produto 1', preco: 10.0 },
+  { id: 2, nome: 'Produto 2', preco: 20.0 },
+  { id: 3, nome: 'Produto 3', preco: 30.0 },
+  { id: 4, nome: 'Produto 4', preco: 40.0 }
+];
+
+app.get('/produtos', (req, res) => {
+    res.json(produtos);
+});
+
+//Inicializa o servidorna porta 3000
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta http://localhost:3000');
+});
---
+++ todo.md
@@ -0,0 +1,5 @@
+### Para Casa
+- API completa
+    GET, POST, PUT, DELETE
+    - Armazenar em cookies
+    - Armazenar em BD Local
